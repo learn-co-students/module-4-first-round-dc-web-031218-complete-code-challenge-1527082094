ef60cfad3cf825c7b30f6d68949f32cc027f6695
azeelleeza <leezaluncheon@gmail.com>
2018-05-23 10:57:12 -0400

    done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,20 +1,18 @@
 import React from "react";
 import BotCard from "../components/BotCard";
 
-class BotCollection extends React.Component {
-  //your code here
-
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
-  }
-
+const BotCollection = props => {
+  const handleClick = e => {
+    let id = e.target.closest(".column").id;
+    props.changeDisplay(parseInt(id, 10));
+  };
+  return (
+    <div className="ui four column grid" onClick={handleClick}>
+      <div className="row">
+        {props.bots.map(bot => <BotCard key={bot.id} bot={bot} />)}
+      </div>
+    </div>
+  );
 };
 
 export default BotCollection;
