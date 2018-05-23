543f48485790558fbe0b255792be09d957e906c1
Austin Wolfe <wolfe_austin@yahoo.com>
2018-05-23 10:58:49 -0400

    code challenge

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,17 +4,23 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+  render() {
+    console.log(this.props);
+    return (
+      <div className="ui four column grid">
+        <div className="row">
+          {this.props.bots.map(bot => (
+            <BotCard
+              onClick={this.props.addToArmy}
+              key={bot.id}
+              bot={bot}
+              value={bot.id}
+            />
+          ))}
+        </div>
+      </div>
+    );
   }
-
-};
+}
 
 export default BotCollection;
